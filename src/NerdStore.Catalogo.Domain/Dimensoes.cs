a0e867c9c45c2c365ef5e54f59704c6a88867ae3
Carlos Henrique <carloshp@outlook.com>
2021-02-04 22:00:03 -0300

    objeto de valor, adicionado a classe Dimensoes dentro da entidade Produto.

---
+++ src/NerdStore.Catalogo.Domain/Dimensoes.cs
@@ -0,0 +1,37 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Dimensoes
+    {
+        public decimal Altura { get; private set; }
+        public decimal Largura { get; private set; }
+        public decimal Profundidade { get; private set; }
+
+        public Dimensoes(decimal altura, decimal largura, decimal profundidade)
+        {
+            Altura = altura;
+            Largura = largura;
+            Profundidade = profundidade;
+
+            Validar();
+        }
+
+        private void Validar()
+        {
+            Validacoes.ValidarSeMenorIgualMinimo(Altura, 1, "O campo Altura deve ser no minimo maior ou igual a 1!");
+            Validacoes.ValidarSeMenorIgualMinimo(Largura, 1, "O campo Largura deve ser no minimo maior ou igual a 1!");
+            Validacoes.ValidarSeMenorIgualMinimo(Profundidade, 1, "O campo Profundidade deve ser no minimo maior ou igual a 1!");
+        }
+
+        public string DescricaoFormatada()
+        {
+            return $"LxAxP: {Largura} x {Altura} x {Profundidade}";
+        }
+
+        public override string ToString()
+        {
+            return DescricaoFormatada();
+        }
+    }
+}
